08621e50a6d354e6613c87093071c3644cec075a
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2023-09-06 02:07:45 +0000

    core elements complete

--- preprocessors/title.js
+++ preprocessors/title.js
@@ -2,11 +2,14 @@ var JSDOM = require('jsdom')
 var document = new JSDOM('<!DOCTYPE html><html><head id="head"></head><body id="__main"></body></html>').window.document
 module.exports = (comp, obj) => {
     document.getElementById('__main').innerHTML = comp
-    var elm = document.getElementsByClassName('progress')[0]
-    if(obj.percent){
-        elm.innerText=obj.percent
-        elm.value=obj.percent.replaceAll('%','')
+    var elm = document.getElementsByClassName('title')[0]
+    if(obj.subtitle){
+        elm.innerText=obj.subtitle
+        elm.className = 'subtitle'
+    }else{
+        elm.innerText = obj.title
     }
+    elm.className = `${elm.getAttribute("class")} is${obj.size}`
     obj.classes.forEach(className => {
         elm.className = `${elm.getAttribute("class")} ${className}`
     })
